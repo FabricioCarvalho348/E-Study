8756333fc8d65f56383bee35d6155f8114cef9a0
Fabricio Carvalho <fabricioccarvalho348@gmail.com>
2025-08-31 14:25:25 -0300

    feat: Configuring the docker environment in the backend

--- src/backend/EStudy.Api/Program.cs
+++ src/backend/EStudy.Api/Program.cs
@@ -1,16 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "E-Study API", Version = "v1" });
+});
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(
+    c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "E-Study API V1");
+        c.RoutePrefix = string.Empty;
+    });
+
+if (app.Environment.IsProduction())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+app.MapGet("/health", () => Results.Ok(new { status = "API is running", time = DateTime.UtcNow }));
 
 app.Run();
